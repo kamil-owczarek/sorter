4000b9f75a8ecd9c7480d914f65642e32f2a1d2f
Kamil Owczarek <owczarek9721@gmail.com>
2020-08-02 21:58:00 +0200

    Refactored code

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# sorter
-Bubble and quick sort with time of executing sorts
+## Table of contents
+* [General info](#general-info)
+* [Technologies](#technologies)
+* [Setup](#setup)
+
+## General info
+Bubble and Quick sort algorithm with time of executing sorts.
+
+In config.ini file you can select number of elements, step in sort and number of iteration for each algorithm.
+
+In input.txt file is 30000 random numbers using for sort.
+
+## Technologies
+Project is created with:
+* Python version: 3.7
+
+## Setup
+Compile and run with:
+$ python sorter.py
+
--- sorter.py
+++ sorter.py
@@ -1,114 +1,95 @@
 import datetime
 from configparser import ConfigParser
-config=ConfigParser()
+
+config = ConfigParser()
 config.read('config.ini')
 
-elementsQS=int(config['quicksort']['elements'])
-stepQS=int(config['quicksort']['step'])
-iterQS=int(config['quicksort']['iter'])
+quickSortElements = int(config['quicksort']['elements'])
+quickSortStep = int(config['quicksort']['step'])
+quickSortIteration = int(config['quicksort']['iter'])
 
-elementsBS=int(config['bubblesort']['elements'])
-stepBS=int(config['bubblesort']['step'])
-iterBS=int(config['bubblesort']['iter'])
+bubbleSortElements = int(config['bubblesort']['elements'])
+bubbleSortStep = int(config['bubblesort']['step'])
+bubbleSortIteration = int(config['bubblesort']['iter'])
+
+times = []
 
 
-times=[]
-
-
-def writeBS(list):
-    f= open("output_BS.txt","a")
-    f.writelines(str(list))
-    f.write('\n')
+def open_input_file(input_file):
+    f = open(input_file, "r")
+    data = eval(f.readline())
     f.close()
+    return data
 
-def writeQS(list):
-    f= open("output_QS.txt","a")
-    f.writelines(str(list))
-    f.write('\n')
-    f.close()
 
-def bubblesort(list,n):
-
+def bubble_sort_algorithm(element_list, n):
     for i in range(n):
-        for j in range(n-i-1):
-            if list[j]>list[j+1]:
-               list[j],list[j+1]=list[j+1],list[j]
-    return list
+        for j in range(n - i - 1):
+            if element_list[j] > element_list[j + 1]:
+                element_list[j], element_list[j + 1] = element_list[j + 1], element_list[j]
 
 
-
-def quicksort(list,l=0,r=None):
+def quick_sort_algorithm(element_list, l=0, r=None):
     if r is None:
-        r=len(list)-1
-    i,j=l,r
-    if(l+r)%2==0:
-        mid=(l+r)/2
+        r = len(element_list) - 1
+    i, j = l, r
+    if (l + r) % 2 == 0:
+        mid = (l + r) / 2
     else:
-        mid=(l+r+1)/2
-    pivot=list[int(mid)]
-    while i<=j:
-        while list[i]<pivot:i+=1
-        while list[j]>pivot:j-=1
-        if i<=j:
-            list[i],list[j]=list[j],list[i]
-            i+=1;j-=1
-    if l<j:quicksort(list,l,j)
-    if r>i:quicksort(list,i,r)
-
-
+        mid = (l + r + 1) / 2
+    pivot = element_list[int(mid)]
+    while i <= j:
+        while element_list[i] < pivot: i += 1
+        while element_list[j] > pivot: j -= 1
+        if i <= j:
+            element_list[i], element_list[j] = element_list[j], element_list[i]
+            i += 1
+            j -= 1
+    if l < j: quick_sort_algorithm(element_list, l, j)
+    if r > i: quick_sort_algorithm(element_list, i, r)
 
 
 def main():
-    ans=True
+    data = open_input_file("input.txt")
+    ans = True
     while ans:
         print("""
-              Wybierz opcje:
-          1. Bubble sort
-          2. Quick sort
-          w. Wyjdź
+              Choose option:
+          1. Bubble Sort
+          2. Quick Sort
+          w. Exit
           """)
-        ans=input()
-        if ans=="1":
-            # bubble
-            step = stepQS
-            f = open("input.txt", "r")
-            data = eval(f.readline())
-            f.close()
-            print("Sortuje")
-            for i in range(0, elementsQS + step, step):
-                for x in range(iterQS):
-                    start = datetime.datetime.now()
-                    bubblesort(data, i)
-                    end = (datetime.datetime.now() - start)
-                    times.append(int(end.total_seconds() * 1000))
-                writeBS(times)
-                times.clear()
+        ans = input()
+
+        if ans == "1":
+            print("Sorting")
+            start = datetime.datetime.now()
+            for i in range(0, bubbleSortElements + bubbleSortStep, bubbleSortStep):
+                for x in range(bubbleSortElements):
+                    bubble_sort_algorithm(data, i)
+            end = (datetime.datetime.now() - start)
             print(data)
-            print("Posortowane!")
-        elif ans=="2":
-            #quick
-            step = stepQS
-            f = open("input.txt", "r")
-            data = eval(f.readline())
-            f.close()
-            print("Sortuje")
-            for j in range(0, elementsQS + step, step):
-                for i in range(iterQS):
-                    start = datetime.datetime.now()
-                    quicksort(data, 0, j - 1)
-                    end = (datetime.datetime.now() - start)
-                    times.append(int(end.total_seconds() * 1000))
-                writeQS(times)
-                times.clear()
+            print("Sorted in " + str(end.seconds) + " seconds.")
+            times.clear()
+
+        elif ans == "2":
+            print("Sorting")
+            start = datetime.datetime.now()
+            for j in range(0, quickSortElements + quickSortStep, quickSortStep):
+                for i in range(quickSortIteration):
+                    quick_sort_algorithm(data, 0, j - 1)
+            end = (datetime.datetime.now() - start)
             print(data)
-            print("Posortowane!")
-        elif ans=="w":
-            print("Do zobaczenia")
+            print("Sorted in " + str(end.seconds) + " seconds.")
+            times.clear()
+
+        elif ans == "w":
+            print("Bye Bye")
             break
 
-        elif ans!="":
-            print("Brak opcji")
-
-main()
+        elif ans != "":
+            print("Unknown option")
 
 
+if __name__ == '__main__':
+    main()
